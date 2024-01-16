b84cd70bbca5e473bf57ec49cb80233fbfdf166c
laurenCho9 <lauren.choux@gmail.com>
2024-01-17 05:14:36 +0900

    fix: form의 체크박스 버그 수정
    체크박스는 여러개 생성되는데 고정된 id 값과 hrmlFor의 관계로 인해 label 클릭 시 첫번째 목록만 체크되는 버그 수정

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 import styles from "./TodoList.module.css";
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    { id: "123", text: "장보기", status: "active" },
-    { id: "124", text: "공부하기", status: "active" },
-  ]);
+  const [todos, setTodos] = useState(readTodosFromLocalStorage);
 
   const handleAdd = (todo) => setTodos([...todos, todo]);
   const handleUpdate = (updated) =>
@@ -16,6 +13,9 @@ export default function TodoList({ filter }) {
   const handleDelete = (deleted) =>
     setTodos(todos.filter((t) => t.id !== deleted.id));
 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   const filtered = getFilteredItems(todos, filter);
   return (
     <section className={styles.container}>
@@ -32,6 +32,12 @@ export default function TodoList({ filter }) {
       <AddTodo onAdd={handleAdd} />
     </section>
   );
+}
+
+function readTodosFromLocalStorage() {
+  console.log("readTodosFromLocalStorage", readTodosFromLocalStorage);
+  const todos = localStorage.getItem("todos");
+  return todos ? JSON.parse(todos) : [];
 }
 
 function getFilteredItems(todos, filter) {
